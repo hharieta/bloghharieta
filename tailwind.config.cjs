9224be370a430ca34d721cb50936160006a60c13
Gatovsky <101161255+hharieta@users.noreply.github.com>
2024-06-30 13:04:11 -0500

    Added new fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,14 +4,14 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
-      fontFamily: {
-        sans: [
-          "Bricolage Grotesque Variable",
-          "Inter Variable",
-          "Inter",
-          ...defaultTheme.fontFamily.sans,
-        ]
-      },
+      // fontFamily: {
+      //   sans: [
+      //     "Bricolage Grotesque Variable",
+      //     "Inter Variable",
+      //     "Inter",
+      //     ...defaultTheme.fontFamily.sans,
+      //   ]
+      // },
     },
   },
   plugins: [require("@tailwindcss/typography")],
