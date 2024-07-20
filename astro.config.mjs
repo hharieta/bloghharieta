e17dbbf163bf59bc6964270d896e9617cffe6cd7
Gatovsky <101161255+hharieta@users.noreply.github.com>
2024-07-20 17:24:03 -0500

    vercel analitycs and speed insights actived

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,14 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.hharieta.lat",
   integrations: [tailwind(), mdx(), sitemap(), icon()],
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: { enabled: true }
+  }),
 });
